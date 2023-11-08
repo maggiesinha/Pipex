025f289168ff4de2e3cf1029d2c628217baa6356
Maggie <maggie@MBP-de-orcas.lan>
2023-11-08 11:34:07 +0000

    8nov2023_11h34

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: maggie <maggie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 12:59:53 by maggie            #+#    #+#             */
-/*   Updated: 2023/10/02 13:28:26 by maggie           ###   ########.fr       */
+/*   Updated: 2023/11/08 11:23:18 by maggie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,20 +54,44 @@ char	*find_cmd_path(char *envp[], char *cmd)
 				return (final_path);
 			}
 			i++;
+			free(final_path);
 	}
 	ft_free_strarray(paths);
 	free (cmd);
-	free (final_path);
 	return (NULL);
 }
 
-int	executing_commands(char *cmd, int input_file, int output_file, char *envp[])
+int	executing_commands(char *path, int input_file, int output_file, char *envp[], char *argv[])
 {
-	int	pipefd[2];
-	int	pid;
-	char *output;
+	int	childpid;
+	int	wait_status;
+	char *cmd[2];
 
+	cmd[0] = argv[2];
+	cmd[1] = NULL;
 	
+	if (dup2(input_file, STDIN_FILENO) == -1) {
+        perror("dup2 (input_file)");
+        exit(1);
+    }
+	if (dup2(output_file, STDOUT_FILENO) == -1) {
+        perror("dup2 (output_file)");
+        exit(1);
+    }
+	childpid = fork(); // Create a child process
+    if (childpid == -1)
+	{
+        perror("fork");
+        return 1;
+    }
+	if (childpid == 0)
+		execve(path, cmd, envp);
+	else
+	{
+		waitpid(childpid, &wait_status, 0);
+		printf("Parent process runs after fork\n");
+	}
+	return (0);
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -77,23 +101,21 @@ int	main(int argc, char *argv[], char *envp[])
 	int		output_file;
 	int		i;
 
-	if (argc < 5)
+	if (argc < 4)
 		return (1);
 	input_file = open(argv[1], O_RDONLY);
 	output_file = open(argv[argc - 1], O_WRONLY);
 	i = 2;
 	while (i < argc - 1)
 	{
-		path = find_cmd_path(envp, argv[i++]);
+		path = find_cmd_path(envp, argv[i]);
 		if (!path)
 		{
-			perror(path);
+			perror(argv[i]);
 			return (1);
 		}
-		executing_commands(path, input_file, output_file, envp);
+		executing_commands(path, input_file, output_file, envp, argv);
+		i++;
 	}
-
-	
-
 
 }
